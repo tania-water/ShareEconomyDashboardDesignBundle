60e44c37302c563a4ab3357a9527f9aeee0a624a
Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
2016-10-05 16:36:09 +0200

    Enhancement: Auto focus and submit on pressing enter

--- Resources/public/js/forms-layout.js
+++ Resources/public/js/forms-layout.js
@@ -1,11 +1,20 @@
 'use strict';
 
 jQuery(document).ready(function ($) {
+    // auto focus on the first editable elemnt in the form
+    $('.dev-main-form-container form :input:enabled:visible:not([readonly], button, :file):first').focus();
+    // submit form when pressing enter
+    $('.dev-main-form-container form').on('keyup', function (e) {
+        if (e.keyCode === 13) {
+            $('.dev-main-form-container form').submit();
+        }
+    });
+    // design buttons actions
     $('.dev-form-save-button').on('click', function () {
         $('.dev-main-form-container form').submit();
     });
     $('.dev-form-reset-button').on('click', function () {
-        $('.dev-main-form-container form')[0].reset();
+        $('.dev-main-form-container form').get(0).reset();
     });
     // global fix for firefox filling password fields
     $('.dev-main-form-container form input[type=password][autocomplete=off]').each(function () {
