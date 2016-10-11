90e0d005604756624d4389bcb681371048b20300
Ola Ali <ola.ali@ibtikar.net.sa>
2016-10-11 15:40:54 +0300

    remove autofocus from first field

--- Resources/public/js/forms-layout.js
+++ Resources/public/js/forms-layout.js
@@ -2,7 +2,7 @@
 
 jQuery(document).ready(function ($) {
     // auto focus on the first editable elemnt in the form
-    $('.dev-main-form-container form :input:enabled:visible:not([readonly], button, :file):first').focus();
+//    $('.dev-main-form-container form :input:enabled:visible:not([readonly], button, :file):first').focus();
     // submit form when pressing enter
     $('.dev-main-form-container form').on('keyup', function (e) {
         if (e.keyCode === 13) {
@@ -15,7 +15,7 @@ jQuery(document).ready(function ($) {
     });
     $('.dev-form-reset-button').on('click', function () {
         $('.dev-main-form-container form').get(0).reset();
-        $('input:checkbox').closest('span').removeClass('checked')
+        $('input:checkbox').uniform('refresh');
     });
     // global fix for firefox filling password fields
     $('.dev-main-form-container form input[type=password][autocomplete=off]').each(function () {
