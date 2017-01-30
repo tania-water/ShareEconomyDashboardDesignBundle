5020f7ccb73bc47475e2b65b2410004859daa5ef
Sarah Mostafa <sarah.marzouk@ibtikar.net.sa>
2017-01-30 12:56:09 +0200

    admin users list

--- Controller/DashboardController.php
+++ Controller/DashboardController.php
@@ -66,26 +66,7 @@ class DashboardController extends Controller
 
     public function __construct()
     {
-        foreach ($this->listColumns as $key => $column) {
-            if (count($column) > 1) {
-                if (isset($column[1]['entity'])) {
-                    if (isset($column[1]['sort'])) {
-                        $this->listSearchColumns[] = $column[1]['sort'];
-                    } else {
-                        $this->listSearchColumns[] = $column[1]['entity'] . '.' . $column[0];
-                    }
-                } else if (isset($column[1]['selectOptionsList'])) {
-                    $className = $this->entityBundle . "\\Entity\\" . $this->className;
-                    $staticFuction = $column[1]['selectOptionsList'];
-                    $this->listColumns[$key][1]['selectOptions'] = call_user_func(array($className, $column[1]['selectOptionsList']));
-                    $this->listSearchColumns[] = $column[0];
-                } else {
-                    $this->listSearchColumns[] = $column[0];
-                }
-            } else {
-                $this->listSearchColumns[] = $column[0];
-            }
-        }
+
     }
 
     /**
@@ -627,7 +608,26 @@ class DashboardController extends Controller
     }
 
     protected function setListParameters(){
-
+        foreach ($this->listColumns as $key => $column) {
+            if (count($column) > 1) {
+                if (isset($column[1]['entity'])) {
+                    if (isset($column[1]['sort'])) {
+                        $this->listSearchColumns[] = $column[1]['sort'];
+                    } else {
+                        $this->listSearchColumns[] = $column[1]['entity'] . '.' . $column[0];
+                    }
+                } else if (isset($column[1]['selectOptionsList'])) {
+                    $className = $this->entityBundle . "\\Entity\\" . $this->className;
+                    $staticFuction = $column[1]['selectOptionsList'];
+                    $this->listColumns[$key][1]['selectOptions'] = call_user_func(array($className, $column[1]['selectOptionsList']));
+                    $this->listSearchColumns[] = $column[0];
+                } else {
+                    $this->listSearchColumns[] = $column[0];
+                }
+            } else {
+                $this->listSearchColumns[] = $column[0];
+            }
+        }
     }
 
     protected function setEditOptions(){
